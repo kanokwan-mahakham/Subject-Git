a822b72298ce484ab4507a1ca764ab67529fd2e4
kanokwan-mahakham <119473607+kanokwan-mahakham@users.noreply.github.com>
2023-10-04 09:54:32 +0700

    k

---
+++ Utils.js
@@ -0,0 +1,9 @@
+function helloworld(){
+    return "hello world";
+}
+
+function one_plus(x){
+    return 1+x;
+}
+
+module.exports = (helloworld, one_plus);
--- test_function.js
+++ test_function.js
@@ -1,6 +1,15 @@
-const {helloworld} = require("./Utils");
+const {helloworld, one_plus} = require("./Utils");
 if(helloworld() === "hello world")
 {
+
+}else{
+    console.log(1);
+    return;
+}
+
+if(one_plus(1) === 2)
+{
+
 }else{
     console.log(1);
     return;
